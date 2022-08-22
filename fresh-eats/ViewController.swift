416c1c44c71b2e40f6522ba60d20d5e55ed54ffd
Kevin <kevinkhoit@gmail.com>
2022-08-22 16:58:28 -0400

    Initial Commit

---
+++ fresh-eats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fresh-eats
+//
+//  Created by Kevin Truong on 2022-08-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
